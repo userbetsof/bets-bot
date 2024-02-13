4390ab4fd91e8af495ecd042dbbb5a0bdd50ab23
userbetsof <157900278+userbetsof@users.noreply.github.com>
2024-02-13 19:00:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6055776005:AAGPEEGBcTlKNgKkee1CEUmdAyhBiCvnciw';
 const bot = new TelegramBot(token, {polling: true});
-// Import the functions you need from the SDKs you need
-const initializeApp = require('firebase/app');
-const getAnalytics = require('firebase/analytics');
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyClbeAWi2UWa--jE-rHQmFRDdzAOeRBlYw",
-  authDomain: "bets-17797.firebaseapp.com",
-  projectId: "bets-17797",
-  storageBucket: "bets-17797.appspot.com",
-  messagingSenderId: "868808532411",
-  appId: "1:868808532411:web:aaad617eb6d510157de49e",
-  measurementId: "G-FTRF2W4VWB"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const keyboard = [
     [
       {
